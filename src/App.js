9cc976aa598ed6d1fdfe2f74a2deb3aba7529a96
ooeid <ooeid.omar@gmail.com>
2020-01-13 10:48:57 +0300

    day1 category

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
     <div className="App">
      
      <Category/>
-
-     <List/>
+      
+         <List/>
        
       
 
--- src/components/Category/API.jsx
+++ src/components/Category/API.jsx
@@ -1,6 +1,5 @@
-import {React, Component,   } from "react";
+import React , {Component} from "react";
 
-import { render } from "@testing-library/react";
 import axois from 'axois'
 
 export default class API extends Component{
@@ -8,6 +7,23 @@ export default class API extends Component{
 
     }
     
+    componentDidMount() {
+        axois.get("https://robby.p.rapidapi.com/search.json?country=US&lng=-74.00597&city=New%20York&lat=40.71435&to=2016-08-01T20%253A30%253A00%252B08%253A00&limit=30&distance=10&from=2016-06-30T20%253A30%253A00")
+        .then( resp => {
+            console.log(resp.)
+        })
+        .catch(err => {
+            console.log("Error");
+            
+        })
+
+.then(response => {
+	console.log(response);
+})
+.catch(err => {
+	console.log(err);
+});
+    }
 
     render(){
 

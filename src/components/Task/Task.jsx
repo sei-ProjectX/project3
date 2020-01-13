9d80df258d77ae074a771bda90c22c1673d2cef2
Bayan <bayanalbunayan@gmail.com>
2020-01-13 10:01:15 +0300

    task complaite with submit

--- src/components/Task/Task.jsx
+++ src/components/Task/Task.jsx
@@ -1,7 +1,6 @@
 import React,{Component} from 'react'
 // import Card from 'react-bootstrap/card'
 // import taskDisplay from '../TasksDisplay/TasksDisplay'
-
 class Task extends Component {
     state = {
          info:{

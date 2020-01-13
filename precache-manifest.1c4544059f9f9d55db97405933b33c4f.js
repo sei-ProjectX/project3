a2740095b4218271cac6b324c0269c96c061cdd3
Bayan <bayanalbunayan@gmail.com>
2020-01-13 14:04:10 +0300

    Updates

---
+++ precache-manifest.1c4544059f9f9d55db97405933b33c4f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c9108515733cbdf8ffedaf604a44d7d",
+    "url": "/sei-ProjectX/project3/index.html"
+  },
+  {
+    "revision": "698974bdb78b29a040c3",
+    "url": "/sei-ProjectX/project3/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "adb7358d7a525d0cd01c",
+    "url": "/sei-ProjectX/project3/static/js/2.01230b93.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/sei-ProjectX/project3/static/js/2.01230b93.chunk.js.LICENSE"
+  },
+  {
+    "revision": "698974bdb78b29a040c3",
+    "url": "/sei-ProjectX/project3/static/js/main.cb16ab3d.chunk.js"
+  },
+  {
+    "revision": "24cafc62034eeb496a70",
+    "url": "/sei-ProjectX/project3/static/js/runtime-main.6da8adda.js"
+  }
+]);
